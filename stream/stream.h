18add073582f3e531fe5a24b9a049a3342914df7
Masahiro Ide <imasahiro9@gmail.com>
2012-12-26 10:05:28 +0900

    add stream

---
+++ stream/stream.h
@@ -0,0 +1,15 @@
+#ifndef LOGPOOL_STREAM_H
+#define LOGPOOL_STREAM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct logpool_stream {
+    void *param;
+} stream_t;
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+#endif /* end of include guard */
